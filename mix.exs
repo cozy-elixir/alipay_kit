9935b20b9e2e0f7472ed90c503f2c4dd2823910c
c4710n <c4710n@users.noreply.github.com>
2024-07-12 15:29:47 +0800

    release: bump v0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule AlipayKit.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @description "A kit for Alipay."
   @source_url "https://github.com/cozy-elixir/alipay_kit"
 
